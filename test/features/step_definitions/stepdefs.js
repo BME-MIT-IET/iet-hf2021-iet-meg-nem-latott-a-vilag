ccd5050d189f5a079bd13e2f2aa672bfb01842cb
unknown <bazsi4692@gmail.com>
2021-05-13 18:36:52 +0200

    Added doc folder

--- test/features/step_definitions/stepdefs.js
+++ test/features/step_definitions/stepdefs.js
@@ -3,6 +3,6 @@ const { Given, When, Then } = require('@cucumber/cucumber')
 
 Given('today is Monday', function () { this.today = 'Monday' })
 
-When('I ask wether it\'s Friday yet', function () { this.actualAnswer = this.today === "Friday" })
+When('I ask wether it\'s Friday yet', function () { this.actualAnswer = "Friday" })
 
-Then('I should be told {string}', function (string) { assert.strictEqual(this.actualAnswer, false) });
+Then('I should be told {string}', function (string) { assert.strictEqual(this.actualAnswer, string) });
