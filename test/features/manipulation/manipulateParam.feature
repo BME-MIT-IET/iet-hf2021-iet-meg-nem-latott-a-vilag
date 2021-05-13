8be7818d22ab322ee957c784a4e1774fe27441fb
unknown <bazsi4692@gmail.com>
2021-05-13 22:19:01 +0200

    BDD TESTS: Manipulation

---
+++ test/features/manipulation/manipulateParam.feature
@@ -0,0 +1,25 @@
+Feature: Manipulate url
+    You can add, remove or change parameters of the url
+    
+    Scenario: Add parameter
+    Given an url "http://example.com/"
+    When I add a parameter "param" with the value "9"
+    Then the url will have a parameter with name "param" and value "9"
+    
+    Scenario: Delete parameter
+    Given an url "http://example.com/"
+    And it has a parameter "param" with value "9"
+    When I delete the parameter "param"
+    Then the url won't have the parameter "param"
+
+    Scenario Outline: Change parameter
+    Given an url "<url>"
+    And it has a parameter "<parameter>" with value "<value>"
+    When I change the value of "<parameter>" to "<new-value>"
+    Then the value of "<parameter>" will be "<new-value>" 
+
+    Examples:
+        | url                           | parameter | value     | new-value |
+        | http://example.com/           | paramName | 9         | xy        |
+        | https://google.com/path?a=b   | b         | c         | 3         |
+        | /x/y                          | z         | someValue | new Value |
--- test/features/step_definitions/stepdefs.js
+++ test/features/step_definitions/stepdefs.js
@@ -1,8 +1,20 @@
 const assert = require('assert')
 const { Given, When, Then } = require('@cucumber/cucumber')
+const Url = require('../../../url.min.js');
 
-Given('today is Monday', function () { this.today = 'Monday' })
 
-When('I ask wether it\'s Friday yet', function () { this.actualAnswer = "Friday" })
+Given('an url {string}', (url) => { this.url = new Url(url) })
 
-Then('I should be told {string}', function (string) { assert.strictEqual(this.actualAnswer, string) });
+Given('it has a parameter {string} with value {string}', (parameter, value) => { this.url.query[parameter] = value })
+
+When('I delete the parameter {string}', (parameter) => { delete this.url.query[parameter] })
+
+When('I add a parameter {string} with the value {string}', (parameter, value) => { this.url.query[parameter] = value })
+
+When('I change the value of {string} to {string}', (parameter, value) => { this.url.query[parameter] = value })
+
+Then('the url will have a parameter with name {string} and value {string}', (parameter, value) => { assert.strictEqual(this.url.query[parameter], value) })
+
+Then('the url won\'t have the parameter {string}', (parameter) => { assert.strictEqual(this.url.query[parameter], undefined) })
+
+Then('the value of {string} will be {string}', (parameter, value) => { assert.strictEqual(this.url.query[parameter], value) })
