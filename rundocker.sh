b9389f3d9a6d236335d3240344d4f0e291e771d4
Máté Nagy <nmate980829@gmail.com>
2021-05-15 02:53:13 +0200

    add little convenience to rundocker helper script

--- rundocker.sh
+++ rundocker.sh
@@ -3,4 +3,8 @@
 docker build -t domurl-test .
 docker container ls 
 docker rm -f domurl-test
-docker run --name domurl-test -p 8081:8081 domurl-test
+docker run -d --name domurl-test -p 8081:8081 domurl-test
+
+# for launching the page in the browser linux-only
+
+xdg-open http://localhost:8081/
